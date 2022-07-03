0692101a16ff5f8038c0bc03d7a128fe1ed44a4d
nkono <nkono209@gmail.com>
2022-07-03 13:31:35 +0200

    Arrested at 43rd video

--- backend/controllers/users/authController.js
+++ backend/controllers/users/authController.js
@@ -5,137 +5,220 @@ const sendToken = require("../../utils/jwtToken");
 const sendEmail = require("../../utils/sendEmail");
 const crypto = require("crypto");
 
-
-//Register a user 
+//Register a user
 module.exports.registerUser = catchAsyncError(async (req, res, next) => {
-    const { name, email, password } = req.body
-    const user = await userModel.create({ name, email, password,
-         avatar: {
-            public_id: 'zzz',
-            url: 'zzzzzzzzzzzzzzzzzzzzzzzz'
-        } 
-    });
-    /* const token = user.getJwtToken();
+  const { name, email, password } = req.body;
+  const user = await userModel.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "zzz",
+      url: "zzzzzzzzzzzzzzzzzzzzzzzz",
+    },
+  });
+  /* const token = user.getJwtToken();
     return res.status(200).json({
         success: true,
         token
     }) */
 
-    //Replaced by sendToken function
-    return await sendToken(user, 200, res)
-})
+  //Replaced by sendToken function
+  return await sendToken(user, 200, res);
+});
 
-//login user 
+//login user
 module.exports.loginUser = catchAsyncError(async (req, res, next) => {
-    const { email, password } = req.body;
-    if(!email || !password){
-        return next(new ErrorHandler("Please enter email and password", 400));
-    }
-    //Finding user in database
-    const user = await userModel.findOne({email}).select("+password");
-    if(!user){
-        return next(new ErrorHandler("Invalid email or Password", 401));
-    }
-    //check if password is correct or not
-    const isPasswordMatched = await user.comparePassword(password);
-    if(!isPasswordMatched){
-        return next(new ErrorHandler("Invalid Password", 401));
-    } 
- /*    const token = user.getJwtToken();
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email and password", 400));
+  }
+  //Finding user in database
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or Password", 401));
+  }
+  //check if password is correct or not
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Password", 401));
+  }
+  /*    const token = user.getJwtToken();
     return res.status(200).json({
         success: true,
         token
     }) */
 
-     //Replaced by sendToken function
-    return await sendToken(user, 200, res)
-})
+  //Replaced by sendToken function
+  return await sendToken(user, 200, res);
+});
 
 //Forgot password send email
 module.exports.forgotPasswordUser = catchAsyncError(async (req, res, next) => {
-    const user = await userModel.findOne({email: req.body.email});
-    if(!user){
-        return next(new ErrorHandler("Email address not found", 404))
-    }
-    //Get reset token
-    const resetToken = user.getResetPasswordToken();
-    await user.save({validateBeforSave: false})
-    //create reset password url
-    const resetUrl = `${req.protocol}://${req.get('host')}/api/shopping/reset/password/${resetToken}`
-    const message = `Your password reset link is \n\n${resetUrl}`;
-    try {
-        await sendEmail({
-            email: user.email,
-            subject: "Shopping Password Recovery",
-            message
-        })
-        return res.status(200).json({
-            success: true,
-            message: `Email sent to ${user.email}`
-        })
-        
-    } catch (error) {
-        user.resetPasswordToken = undefined;
-        user.resetPasswordExpire = undefined;
-        await user.save({validateBeforeSave: false})
-        return next(new ErrorHandler(error.message, 500))
-        
-    }
-})
+  const user = await userModel.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new ErrorHandler("Email address not found", 404));
+  }
+  //Get reset token
+  const resetToken = user.getResetPasswordToken();
+  await user.save({ validateBeforSave: false });
+  //create reset password url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/shopping/reset/password/${resetToken}`;
+  const message = `Your password reset link is \n\n${resetUrl}`;
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Shopping Password Recovery",
+      message,
+    });
+    return res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email}`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
 
 //Reset password for a new
 module.exports.resetPasswordUser = catchAsyncError(async (req, res, next) => {
-    //Hash URL token 
-    const resetPasswordToken = crypto.createHash("sha256").update(req.params.token).digest("hex");
-    const user = await userModel.findOne({
-        resetPasswordToken,
-        resetPasswordExpire: {$gt: Date.now()}
-    })
-    if(!user){
-        return next(new ErrorHandler("Reset password Invalid Or expired", 400))
-    }
-    if(req.body.password !== req.body.confirmPassword){
-        return next(new ErrorHandler(`Password doesn't match`, 400));
-    }
-    //Setup a new  password
-    user.password = req.body.password;
-    user.resetPasswordToken = undefined;
-    user.resetPasswordExpire = undefined;
-    await user.save();
-    sendToken(user,200,res);
-})
+  //Hash URL token
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+  const user = await userModel.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+  if (!user) {
+    return next(new ErrorHandler("Reset password Invalid Or expired", 400));
+  }
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler(`Password doesn't match`, 400));
+  }
+  //Setup a new  password
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+  await user.save();
+  sendToken(user, 200, res);
+});
 
 //logout user
 module.exports.logoutUser = catchAsyncError(async (req, res, next) => {
-    await res.cookie('token', null, { 
-        expires: new Date(Date.now()), 
-        httpOnly: true
-    })
-    return res.status(200).json({  
-        success: true,
-        message: "Logged out"
-    }) 
-})
+  await res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  return res.status(200).json({
+    success: true,
+    message: "Logged out",
+  });
+});
 
 //Get currently logged in user details
 module.exports.getUserProfile = catchAsyncError(async (req, res, next) => {
-    const user = await userModel.findById(req.user.id);
-    return res.status(200).json({
-        success: true,
-        user
-    })
-})
+  const user = await userModel.findById(req.user.id);
+  return res.status(200).json({
+    success: true,
+    user,
+  });
+});
 
 //Update change password
 module.exports.updateUserPassword = catchAsyncError(async (req, res, next) => {
-    const user = await userModel.findById(req.user.id).select("+password");
-    //check previous user password
-    const isMatched = await user.comparePassword(req.body.oldPassword);
-    if(!isMatched){
-        return next(new ErrorHandler("Old password is incorrect", 400))
-    }
-    user.password = req.body.password;
-    await user.save()
-    sendToken(user, 200, res);
+  const user = await userModel.findById(req.user.id).select("+password");
+  //check previous user password
+  const isMatched = await user.comparePassword(req.body.oldPassword);
+  if (!isMatched) {
+    return next(new ErrorHandler("Old password is incorrect", 400));
+  }
+  user.password = req.body.password;
+  await user.save();
+  sendToken(user, 200, res);
+});
+
+//update user profil
+module.exports.updateUserProfil = catchAsyncError(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  //update avatar:: todo
+
+  const user = await userModel.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  return res.status(200).json({
+    success: true,
+    message: `Your profile is updated`,
+  });
+});
+
+//Get all users
+module.exports.getAllUser = catchAsyncError(async (req, res, next) => {
+  const users = await userModel.find();
+  if (!users) {
+    return next(new ErrorHandler("No users found", 404));
+  }
+  return res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+//Get a single user details
+module.exports.getSingleUser = catchAsyncError(async (req, res, next) => {
+  const user = await userModel.findById(req.params.id);
+  if (!user) {
+    return next(new ErrorHandler(`User not found: ${req.params.id}`, 404));
+  }
+  return res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+//Update user
+module.exports.updateUserInfo = catchAsyncError(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
+  const user = await userModel.findByIdAndUpdate(req.params.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  if(!user){
+    return next(new ErrorHandler(`User not found: ${req.params.id}`, 404))
+  }
+  return res.status(200).json({
+    success: true,
+    message: `User is updated`
+  })
+});
+
+//Delete user
+module.exports.deleteUser = catchAsyncError(async (req, res, next)=>{
+  const user = userModel.findById(req.params.id);
+  if(!user){
+    return next(new ErrorHandler(`User not found: ${req.params.id}`, 404))
+  }
+  //Remove avatar =>todo
+
+  await user.remove()
+  return res.status(200).json({
+    success: true,
+    message: `User is deleted`
+  })
 })
- 
--- backend/routes/users/auth.js
+++ backend/routes/users/auth.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { isAuthenticatedUser } = require("../../middlewares/authCheck");
+const {
+  isAuthenticatedUser,
+  authorizeRoles,
+} = require("../../middlewares/authCheck");
+
 const {
   registerUser,
   loginUser,
@@ -9,16 +13,31 @@ const {
   resetPasswordUser,
   getUserProfile,
   updateUserPassword,
+  updateUserProfil,
+  getAllUser,
+  getSingleUser, updateUserInfo, deleteUser
 } = require("../../controllers/users/authController");
 
 router.route("/register").post(registerUser);
 router.route("/login").get(loginUser);
-router.route("/logout").get(logoutUser); 
+router.route("/logout").get(logoutUser);
 router.route("/password/forgot").post(forgotPasswordUser);
 router.route("/reset/password/:token").put(resetPasswordUser);
 router.route("/me/profile").get(isAuthenticatedUser, getUserProfile);
+router.route("/password/update").put(isAuthenticatedUser, updateUserPassword);
+router.route("/me/profile/update").put(isAuthenticatedUser, updateUserProfil);
 router
-  .route("/password/update")
-  .put(isAuthenticatedUser, updateUserPassword);
+  .route("/admin/users/all")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUser);
+router
+  .route("/admin/users/one/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser);
+router
+  .route("/admin/user/update/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserInfo);
+router
+  .route("/admin/user/delete/:id")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 module.exports = router;
+
